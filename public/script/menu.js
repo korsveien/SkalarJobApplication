a28dec9aac490fce4d50560cd89dae0a32cbe2d9
Robin Heggelund Hansen <skinneyz89@gmail.com>
2013-08-06 21:19:52 +0200

    Moved project into public folder, ready to be used in node.js app.

---
+++ public/script/menu.js
@@ -0,0 +1,101 @@
+var fja = fja || {};
+
+fja.menu = (function () {
+    "use strict";
+
+    var _active_page_id = "",
+        _active_menuitem_element = "";
+
+
+    return {
+        changeActivePage: _change_active_page,
+        changeActiveMenuItem: _change_menuitem_to_active,
+        bindMenuItems: _bind_menu_items
+    };
+
+
+    function _change_active_page(pageID) {
+        var pageToActivate = document.getElementById(pageID),
+            pageToDeactivate = document.getElementById(_active_page_id);
+
+        if (_active_page_id === pageID) {
+            return;
+        } else if (pageToDeactivate !== null) {
+            _fade_out_page(pageToDeactivate, pageToActivate);
+        } else {
+            _fade_in_page(pageToActivate);
+        }
+
+        _active_page_id = pageID;
+    }
+
+    function _bind_menu_items() {
+        var menuItems = document.getElementsByClassName("menuItem"),
+            index,
+            elementID,
+            divIdToBind;
+
+        for (index = 0; index < menuItems.length; index++) {
+            elementID = menuItems[index].id;
+            divIdToBind = elementID.replace("Item", "");
+
+            menuItems[index].onclick = (function (menuItemID, pageElementID) {
+                return function() {
+                    fja.menu.changeActiveMenuItem(menuItemID);
+                    fja.menu.changeActivePage(pageElementID);
+                };
+            })(elementID, divIdToBind);
+        }
+    }
+
+    function _change_menuitem_to_active(menuElementID) {
+        var menuElement = document.getElementById(menuElementID);
+
+        if (_active_menuitem_element !== "")
+            _active_menuitem_element.className = "menuItem";
+
+        menuElement.className = "activeMenuItem";
+        _active_menuitem_element = menuElement;
+    }
+
+    function _fade_out_page(pageToFadeOut, pageToFadeIn) {
+        var opacityStep = 2,
+            fadeTimeInMs = 50;
+
+        if (pageToFadeOut.style.opacity === "") {
+            pageToFadeOut.style.opacity = 1;
+        } else if (pageToFadeOut.style.opacity <= 0) {
+            pageToFadeOut.style.display = "none";
+            _fade_in_page(pageToFadeIn);
+            return;
+        }
+
+        // Workaround javascript bug by multiplying by 10 and deviding by 10
+        pageToFadeOut.style.opacity = ((pageToFadeOut.style.opacity * 10) - opacityStep) / 10;
+
+        setTimeout(function() {
+            _fade_out_page(pageToFadeOut, pageToFadeIn);
+        }, fadeTimeInMs);
+    }
+
+    function _fade_in_page(pageToFadeIn) {
+        var opacityStep = 2,
+            fadeTimeInMs = 50;
+
+        if (pageToFadeIn.style.display !== "block")
+            pageToFadeIn.style.display = "block";
+
+        if (pageToFadeIn.style.opacity === "") {
+            pageToFadeIn.style.opacity = 0;
+        } else if (pageToFadeIn.style.opacity >= 1) {
+            return;
+        }
+
+        // Workaround javascript bug by multiplying by 10 and deviding by 10
+        pageToFadeIn.style.opacity = ((pageToFadeIn.style.opacity * 10) + opacityStep) / 10;
+
+        setTimeout(function() {
+            _fade_in_page(pageToFadeIn);
+        }, fadeTimeInMs);
+    }
+})();
---
+++ public/script/sprite.js
@@ -0,0 +1,94 @@
+var fja = fja || {};
+
+fja.createSprite = function(filename, fps, animations, frames) {
+    "use strict";
+
+    var _image = new Image(),
+        _x = 0,
+        _y = 0,
+        _current_frame = 0,
+        _current_animation = 0,
+        _frame_width = 0,
+        _frame_height = 0,
+        _need_render = true,
+        _frame_interval = 0,
+        _time_since_last_frame = 0;
+
+    _set_image_src(filename);
+    _set_frame_interval(fps);
+
+
+    return {
+        get x() { return _x; },
+        get y() { return _y; },
+        get width() { return _frame_width * fja.screen.canvasWidthScale; },
+        get height() { return _frame_height * fja.screen.canvasHeightScale; },
+
+        render: _render,
+        move: _move,
+        setAnimation: _set_animation
+    };
+
+
+    function _render() {
+        var widthDrawSize = _frame_width * fja.screen.canvasWidthScale,
+            heightDrawSize = _frame_height * fja.screen.canvasHeightScale;
+
+        fja.canvasContext.drawImage(_image,
+                                    _get_x_offset(), _get_y_offset(), _frame_width, _frame_height,
+                                    _x, _y, widthDrawSize, heightDrawSize);
+        _need_render = false;
+        _set_next_frame();
+    }
+
+    function _move(x, y) {
+        _x = x;
+        _y = y;
+        _need_render = true;
+    }
+
+    function _set_animation(animationAsNumber) {
+        if (animationAsNumber >= animations)
+            throw { name: "Animation out of range" };
+        else if (animationAsNumber < 0)
+            throw { name: "Animation out of range" };
+
+        _current_animation = animationAsNumber;
+        _need_render = true;
+    }
+
+    function _set_image_src(filename) {
+        _image.src = "img/" + filename;
+        _image.onload = _set_frame_dimensions;
+    }
+
+    function _set_frame_interval(fps) {
+        _frame_interval = 1000 / fps;
+    }
+
+    function _set_frame_dimensions() {
+        _frame_width = _image.width / frames;
+        _frame_height = _image.height / animations;
+    }
+
+    function _get_x_offset() {
+        return (_current_frame * _frame_width);
+    }
+
+    function _get_y_offset() {
+        return (_current_animation * _frame_height);
+    }
+
+    function _set_next_frame() {
+        var currentTime = Date.now();
+        var timeSinceLastUpdate = currentTime - _time_since_last_frame;
+
+        if (timeSinceLastUpdate > _frame_interval) {
+            _current_frame += 1;
+            _time_since_last_frame = currentTime;
+
+            if (_current_frame >= frames)
+                _current_frame = 0;
+        }
+    }
+};
