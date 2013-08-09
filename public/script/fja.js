bd538f1cad3b4921c63e76c453eb18cabdd3fca5
Robin Heggelund Hansen <skinneyz89@gmail.com>
2013-08-09 22:45:58 +0200

    Minor refactoring.

--- public/script/fja.js
+++ public/script/fja.js
@@ -1,13 +1,10 @@
 var fja = fja || {};
 
-fja.canvas = undefined;
-fja.canvasContext = undefined;
-
 fja.init = function() {
     "use strict";
 
-    fja.canvas = document.getElementById('canvas');
-    fja.canvasContext = fja.canvas.getContext('2d');
+    fja.canvas = document.getElementById("canvas");
+    fja.canvasContext = fja.canvas.getContext("2d");
     fja.screen.calculateCanvasScale();
 
     setInterval(fja.screen.render, 10);
