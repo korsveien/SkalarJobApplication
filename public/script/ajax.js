651ea19be7a0981e794a703ac869e4bff8e4db83
Robin Heggelund Hansen <skinneyz89@gmail.com>
2013-08-20 21:05:15 +0200

    Parse to JSON if receiving JSON object.

--- public/script/ajax.js
+++ public/script/ajax.js
@@ -1,13 +1,19 @@
 var sja = sja || {};
 
-sja.ajax = function(method, url, res) {
+sja.ajax = function(method, url, cb) {
     "use strict";
 
     var req = new XMLHttpRequest(),
         nonCacheURL = url + "?" + (new Date()).getTime();
 
     req.onload = function() {
-        res(this.status, this.response);
+        var res = this.response;
+
+        if (this.getResponseHeader("Content-Type") == "application/json") {
+            res = JSON.parse(res);
+        }
+
+        cb(this.status, res);
     };
 
     req.open(method, nonCacheURL, true);
